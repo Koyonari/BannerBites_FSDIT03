c0f8a5d950a078f43a9cc22d9c70696ac63604ab
An Yong Shyan <yongshyan.an@gmail.com>
2024-10-25 13:01:48 +0800

    fix: routing for ad

--- src/components/Ad/Ad.jsx
+++ src/components/Ad/Ad.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import "../../css/index.css";
+import layout from "../../layout";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { Routes, Route, Link } from "react-router-dom";
 import AdCanvas from "../AdCanvas/AdCanvas";
 import AdViewer from "../AdViewer/AdViewer";
-import "../../css/index.css";
-import layout from "../../layout";
 
 const Ad = () => {
   return (
@@ -16,17 +15,17 @@ const Ad = () => {
           <nav>
             <ul>
               <li>
-                <Link to="/ad-canvas">Ad Canvas</Link>
+                <Link to="ad-canvas">Ad Canvas</Link>
               </li>
               <li>
-                <Link to="/ad-viewer">Ad Viewer</Link>
+                <Link to="ad-viewer">Ad Viewer</Link>
               </li>
             </ul>
           </nav>
           <Routes>
             <Route path="/" element={<AdCanvas />} />
-            <Route path="/ad-canvas" element={<AdCanvas />} />
-            <Route path="/ad-viewer" element={<AdViewer layout={layout} />} />
+            <Route path="ad-canvas" element={<AdCanvas />} />
+            <Route path="ad-viewer" element={<AdViewer layout={layout} />} />
           </Routes>
         </div>
       </DndProvider>
