8db6bf8364d4550c43fb0237ee3c969c10f8828b
An Yong Shyan <yongshyan.an@gmail.com>
2024-10-29 13:27:40 +0800

    fix: remove import of

--- src/components/AdCanvas/AdCanvas.jsx
+++ src/components/AdCanvas/AdCanvas.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 import axios from "axios";
-import "./AdCanvas.css";
 import Sidebar from "./Sidebar";
 import GridCell from "./GridCell";
 import EditModal from "./EditModal";
