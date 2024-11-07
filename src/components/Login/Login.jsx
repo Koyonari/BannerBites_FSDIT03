0e9dc7cfb3d40b37bef869bf919589e7fb9972a4
LiangDingxuan <S10258272@connect.np.edu.sg>
2024-11-07 09:14:34 +0800

    Login file initialise

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = ({ }) => {
+
+  return (
+    <div className="fixed bottom-0 left-0 right-0 top-0 z-[1000] flex items-center justify-center bg-slate-950 bg-opacity-50">
+
+    </div>
+  );
+};
+
+export default Login;
