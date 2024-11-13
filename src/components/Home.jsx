421990b746a34aa0f5f8c48eb6122af9ff66d3b5
An Yong Shyan <yongshyan.an@gmail.com>
2024-11-13 11:56:19 +0800

    style: updated transitions for login + card for userhome

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -35,27 +35,25 @@ const Hero = () => {
     setDarkMode(checked);
   };
 
-
-
   return (
     <div className="flex min-h-screen w-full items-center justify-center bg-white p-4 transition-colors duration-200 dark:bg-black">
       <div className="flex w-full max-w-lg flex-col items-center dark:bg-black">
-        <h1 className="lg:text-5x xl:text-6xll mb-1 text-center text-7xl font-bold text-black dark:text-white md:text-7xl">
+        <h1 className="mb-1 pb-1 text-center text-7xl font-bold text-black dark:text-white md:text-7xl lg:text-5xl xl:text-6xl">
           Banner
           <span className="text-orange-500 outline-3">Bites</span>
         </h1>
 
         <div className="relative flex h-16 w-full max-w-72 justify-center rounded-lg border-2 border-orange-500 bg-white p-4 dark:bg-black md:p-6">
           <div className="flex w-full max-w-xs items-center justify-between">
-          {icons.navbar.map((item) => (
-        <Link
-          key={item.label} // Using label as a unique key here
-          to={item.href}
-          className="flex flex-col items-center"
-        >
-          <item.icon className="h-6 w-6 text-black dark:text-white" />
-        </Link>
-))}
+            {icons.navbar.map((item) => (
+              <Link
+                key={item.label} // Using label as a unique key here
+                to={item.href}
+                className="flex flex-col items-center"
+              >
+                <item.icon className="h-6 w-6 text-black dark:text-white" />
+              </Link>
+            ))}
 
             <DarkModeSwitch
               checked={isDarkMode}
