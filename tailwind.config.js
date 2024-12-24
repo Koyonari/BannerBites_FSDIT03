d5157f387b99894d02b00f4858fd5bffbc14a414
An Yong Shyan <yongshyan.an@gmail.com>
2024-12-25 02:07:31 +0800

    refactor: updated tailwind.config to have high cohesion

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,34 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}", "*.jsx", "App.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          yellow: "rgb(255, 250, 163)",
+          yellow2: "rgb(255, 232, 138)",
+          accent: "rgb(248, 225, 139)",
+        },
+        gray: {
+          100: "rgb(255, 255, 249)",
+          200: "rgb(209, 213, 219)",
+          300: "rgb(156, 163, 175)",
+          400: "rgb(107, 114, 128)",
+          500: "rgb(75, 85, 99)",
+          600: "rgb(46, 46, 46)",
+          700: "rgb(78, 78, 75)",
+        },
+        alert: {
+          red: "rgb(239, 68, 68)",
+          darkRed: "rgb(185, 28, 28)",
+          orange: "rgb(249, 115, 22)",
+          bg: "rgb(254, 242, 242)",
+        },
+        base: {
+          black: "rgb(0, 0, 0)",
+          white: "rgb(255, 255, 255)",
+        },
+      },
+    },
     screens: {
       sl: "375px",
       ss: "415px",
@@ -18,121 +45,106 @@ module.exports = {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
+    function ({ addUtilities, theme }) {
       const newUtilities = {
         ".outline-3": {
           "-webkit-text-stroke": "3px",
-          "-webkit-text-stroke-color": "#000000",
+          "-webkit-text-stroke-color": theme("colors.base.black"),
         },
         ".outline-1": {
           "-webkit-text-stroke": "0.4px",
-          "-webkit-text-stroke-color": "#000000",
+          "-webkit-text-stroke-color": theme("colors.base.black"),
         },
 
-        // Border Colour
+        // Border Colours
         ".primary-border": {
-          borderColor: "rgb(255, 250, 163)",
+          borderColor: theme("colors.primary.yellow"),
         },
         ".secondary-border": {
-          borderColor: "rgb(209 213 219 / 1)",
+          borderColor: theme("colors.gray.200"),
         },
         ".white-border": {
-          borderColor: "rgb(255 255 255 / 1)",
+          borderColor: theme("colors.base.white"),
         },
         ".alert-border": {
-          borderColor: "rgb(239 68 68 / 1)",
+          borderColor: theme("colors.alert.red"),
         },
         ".alert2-border": {
-          borderColor: "rgb(249 115 22 / 1)",
+          borderColor: theme("colors.alert.orange"),
         },
 
-        // Background Colour
+        // Background Colours
         ".pcolor-bg": {
-          backgroundColor: "rgb(255, 250, 163)",
+          backgroundColor: theme("colors.primary.yellow"),
         },
         ".p2color-bg": {
-          backgroundColor: "rgb(255, 232, 138)",
+          backgroundColor: theme("colors.primary.yellow2"),
         },
         ".accent-bg": {
-          backgroundColor: "rgb(248, 225, 139)",
+          backgroundColor: theme("colors.primary.accent"),
         },
         ".scolor-bg": {
-          backgroundColor: "rgb(107 114 128 / 1)",
+          backgroundColor: theme("colors.gray.400"),
         },
         ".gcolor-bg": {
-          backgroundColor: "rgb(209 213 219 / 1)",
+          backgroundColor: theme("colors.gray.200"),
         },
         ".g2color-bg": {
-          backgroundColor: "rgb(156 163 175 / 1)",
+          backgroundColor: theme("colors.gray.300"),
         },
         ".dark-bg": {
-          backgroundColor: "rgb(78 78 75 / 1)",
+          backgroundColor: theme("colors.gray.700"),
         },
         ".light-bg": {
-          backgroundColor: "rgb(255 255 249 / 1)",
+          backgroundColor: theme("colors.gray.100"),
         },
         ".alert-bg": {
-          backgroundColor: "rgb(254 242 242 / 1)",
+          backgroundColor: theme("colors.alert.bg"),
         },
 
-        // Text Colour
+        // Text Colours
         ".pcolor-text": {
-          color: "rgb(255, 250, 163)",
+          color: theme("colors.primary.yellow"),
         },
         ".gcolor-text": {
-          color: "rgb(75 85 99 / 1)",
+          color: theme("colors.gray.500"),
         },
         ".primary-text": {
-          color: "rgb(0 0 0 / 1)",
+          color: theme("colors.base.black"),
         },
         ".secondary-text": {
-          color: "rgb(255 255 255 / 1)",
+          color: theme("colors.base.white"),
         },
         ".light-text": {
-          color: "rgb(46 46 46)",
+          color: theme("colors.gray.600"),
         },
         ".alert-text": {
-          color: "rgb(239 68 68 / 1)",
+          color: theme("colors.alert.red"),
         },
         ".alert2-text": {
-          color: "rgb(185 28 28 / 1)",
+          color: theme("colors.alert.darkRed"),
         },
 
         // Placeholder Colours
-        ".placeholder-primary": {
-          "::placeholder": { color: "rgb(107 114 128)" },
+        ".placeholder-primary::placeholder": {
+          color: theme("colors.gray.400"),
         },
-        ".placeholder-secondary": {
-          "::placeholder": { color: "rgb(156 163 175)" },
+        ".placeholder-secondary::placeholder": {
+          color: theme("colors.gray.300"),
         },
-        ".placeholder-light": {
-          "::placeholder": { color: "rgb(209 213 219)" },
+        ".placeholder-light::placeholder": {
+          color: theme("colors.gray.200"),
         },
-        ".placeholder-dark": {
-          "::placeholder": { color: "rgb(75 85 99)" },
-        },
-
-        // Outline Colours
-        ".primary-outline": {
-          outlineColor: "rgb(253 186 116 / 1)",
-        },
-
-        // Fill Colours
-        ".primary-fill": {
-          fill: "rgb(255 255 255 /1)",
+        ".placeholder-dark::placeholder": {
+          color: theme("colors.gray.500"),
         },
 
         // Ring Colours
-        ".ring-secondary": {
-          "--tw-ring-opacity": "1",
-          "--tw-ring-color": "rgb(59 130 246 / var(--tw-ring-opacity))",
-        },
         ".ring-primary": {
           "--tw-ring-opacity": "1",
-          "--tw-ring-color": "rgb(255 250 163 / var(--tw-ring-opacity))",
+          "--tw-ring-color": `rgb(255 250 163 / var(--tw-ring-opacity))`,
         },
       };
-
       addUtilities(newUtilities, ["responsive", "hover"]);
     },
   ],
